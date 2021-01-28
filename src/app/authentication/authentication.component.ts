6b38f08d21a61f534d007e43139f329ba491314a
Damir <dkolobaric@gmail.com>
2021-01-28 17:07:41 +0100

    authentication form

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
 
@@ -14,7 +14,7 @@ export class AuthenticationComponent implements OnInit {
   isLogin = false;
   buttonContent: string
   authenticationForm: FormGroup;
-  existingEmails = ['dkolobaric@gmail.com'];
+  existingEmails = ['dkolobaric@gmail.com', 'kolobaric@gmail.com'];
 
   constructor(
     private active: ActivatedRoute,
@@ -50,6 +50,15 @@ export class AuthenticationComponent implements OnInit {
     console.log(this.authenticationForm);
   }
 
+  /*
+  TODO: make Async Validator when we get server side
+  */
+
+  /*
+  * needs to  return JS object
+  * it should have any key [] which can be interpreted as string
+  * the value of the key should be boolean
+  */
   alreadyExistingEmails(control: FormControl): { [s: string]: boolean } {
     if (this.existingEmails.indexOf(control.value) !== -1) {
       return { 'existingEmail': true }
