a84c12e282e1d5881d25f69854c3817f49aec73c
Damir <dkolobaric@gmail.com>
2021-01-30 11:27:37 +0100

    FormGroup for settings and amdin commponent

--- src/app/admin/new-user/new-user.component.ts
+++ src/app/admin/new-user/new-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -9,10 +10,29 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class NewUserComponent implements OnInit {
 
   faTimes = faTimes;
+  newUserForm: FormGroup;
+  existingEmails = ['dkolobaric@gmail.com', 'kolobaric@gmail.com'];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.newUserForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email, this.alreadyExistingEmails.bind(this)])
+    })
   }
 
+  onSubmit() {
+    console.log(this.newUserForm);
+  }
+
+  /*
+  TODO: make Async Validator when we get server side
+  */
+
+  alreadyExistingEmails(control: FormControl): { [s: string]: boolean } {
+    if (this.existingEmails.indexOf(control.value) !== -1) {
+      return { 'existingEmail': true }
+    }
+    return null;
+  }
 }
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  settingsForm: FormGroup;
+  existingEmails = ['dkolobaric@gmail.com', 'kolobaric@gmail.com'];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.settingsForm = new FormGroup({
+      'firstName': new FormControl(null),
+      'lastName': new FormControl(null),
+      'image': new FormControl(null),
+      'email': new FormControl(null, [Validators.email, this.alreadyExistingEmails.bind(this)]),
+      'password': new FormControl(null),
+      'confirmPassword': new FormControl(null)
+    })
+  }
+
+  onImagePicked(event: Event) {
+    /* Extract file that was added */
+    const file = (event.target as HTMLInputElement).files;
+    console.log(file);
+    this.settingsForm.patchValue({ 'image': file });
+    this.settingsForm.get('image').updateValueAndValidity();
+  }
+
+  onSubmit() {
+    console.log(this.settingsForm);
+  }
+
+  /*
+  TODO: make Async Validator when we get server side
+  */
+
+  alreadyExistingEmails(control: FormControl): { [s: string]: boolean } {
+    if (this.existingEmails.indexOf(control.value) !== -1) {
+      return { 'existingEmail': true }
+    }
+    return null;
   }
 
 }
